fc5c508ce524f4b914c49a296dea438a62d8a39e
Alexandr Klavsut <alex.kavsut@ya.ru>
2019-08-28 11:34:15 +0300

    Initial Commit

---
+++ MUTestAppTests/MUTestAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MUTestAppTests.swift
+//  MUTestAppTests
+//
+//  Created by Alexandr Klavsut on 28/08/2019.
+//  Copyright © 2019 Alexandr Klavsut. All rights reserved.
+//
+
+import XCTest
+@testable import MUTestApp
+
+class MUTestAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
